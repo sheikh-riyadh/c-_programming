78e6e99bf3d79a91b28ae116d8d7b9b116f339b8
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2024-08-16 09:34:14 +0600

    explore increase size of dynamic array

--- Module 2/increase_size_of_dynamic_array.cpp
+++ Module 2/increase_size_of_dynamic_array.cpp
@@ -6,18 +6,13 @@ int main()
 /* 
 আমরা কিভাবে dynamic array size বৃদ্ধি করতে পারি সেটা দেখবো
 
-১. প্রথমে আমরা একটি arr_1 dynamic array তৈরি করলাম যার size ৩ এবং তার পর আর একটি arr_2 dynamic array তৈরি করলাম.
-২. তার পর আমরা প্রথম arr_1 array
-
-
-
+১. প্রথমে আমরা একটি arr_1 নামে dynamic array তৈরি করলাম যার size ৩ এবং তার পর আর একটি arr_2 নামে dynamic array তৈরি করলাম.
+২. আমরা প্রথম arr_1 array তে ইনপুট নিলাম এবং সাথে arr_2 নামের array তে আমাদের প্রথম arr_1 array এর মান রেখে দিলাম.
+৩. প্রথম arr_1 dynamic array ডিলিট করে দিলাম এখানে dynamic part হল (new int[3]) এবং static part হল (int *arr_1) এইটা.
+৪. এখন আমরা প্রথম arr_1 dynamic array সাইজ বৃদ্ধি করার জন্য আবার ও (new int[5]) করে dynamic ভাবে size বৃদ্ধি করে সেটা arr_1 array তে রেখে দিলাম.
+৫. আমরা যেহেতু (২) ইনপুট নেয়ার সময় arr_2 এর মধ্যে arr_1 এর মান রেখে দিয়ে ছিলাম এখন যেহেতু আমাদের arr_1 এর size ৫ করেছি তাহলে arr_2 এর মান আমরা arr_1 নিয়ে নিলাম.
+৬. এখন আমরা arr_2 ডিলিট করে দিলাম এভাবে করে আমরা arr_1 এর মান ৩ থেকে ৫ করে দিলাম.
  */
-
-
-
-
-
-
 
     int *arr_1 = new int[3];
     int *arr_2 = new int[3];
